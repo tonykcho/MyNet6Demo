176beda2275b39eba5138b41859a3810b37cd96c
tony <tonykcho3527657@gmail.com>
2021-11-10 10:28:59 +0800

    basic endpoint functional test case

---
+++ MyNet6Demo.Api.Tests.Functional/BasicEndpointsTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System;
+
+namespace MyNet6Demo.Api.Tests.Functional
+{
+    [TestClass]
+    public class BasicEndpointsTests
+    {
+        [TestMethod]
+        public async Task BasicTest()
+        {
+            using var app = new BasicEndpointsTestsApp(x => { });
+
+            var httpClient = app.CreateClient();
+
+            var response = await httpClient.GetAsync("/");
+
+            var responseText = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("Hello World!", responseText);
+        }
+    }
+
+    internal class BasicEndpointsTestsApp : WebApplicationFactory<Program>
+    {
+        private readonly Action<IServiceCollection> _serviceOverride;
+
+        public BasicEndpointsTestsApp(Action<IServiceCollection> serviceOverride)
+        {
+            _serviceOverride = serviceOverride;
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            // builder.ConfigureServices();
+
+            return base.CreateHost(builder);
+        }
+    }
+}
