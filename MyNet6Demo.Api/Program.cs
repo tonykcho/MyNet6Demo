c011b616e4d6f8ffbb718afcc63c1e7364646076
tony <tonykcho3527657@gmail.com>
2021-11-10 15:11:27 +0800

    Docker support & serilog support

--- MyNet6Demo.Api/Program.cs
+++ MyNet6Demo.Api/Program.cs
@@ -1,7 +1,38 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log_.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "MyNet6CoreDemo";
+    options.DefaultAuthenticateScheme = "MyNet6CoreDemo";
+    options.DefaultChallengeScheme = "MyNet6CoreDemo";
+}).AddCookie(options =>
+{
+    options.Cookie.Name = "MyNet6CoreDemo.Cookie";
+});
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
+// app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
